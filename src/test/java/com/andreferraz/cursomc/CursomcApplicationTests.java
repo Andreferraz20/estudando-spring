18e9f148f85e82992eaec52ff9a44ff13590917a
Andre Ferraz <luizlopesferraz22@gmail.com>
2020-10-28 16:19:19 -0300

    Projeto criado

---
+++ src/test/java/com/andreferraz/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreferraz.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
